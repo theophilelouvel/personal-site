28979ae33fa48178761f58db3d754bdd716a0f24
Théophile Louvel <louveltheophile@gmail.com>
2020-11-21 06:58:34 +0300

    Switching to Tailwind, step 3

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,7 @@ export default function Layout({ children, home }) {
                     name="description"
                     content="Théophile Louvel"
                 />
+                <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⏄</text></svg>"></link>
             </Head>
             <header>
                 <Nav />
