fdbc7ced91717341e17bef82ccda67e5266b087b
Théophile Louvel <louveltheophile@gmail.com>
2020-11-21 07:05:56 +0300

    Working around absolute import not building

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Nav from '@/components/Nav'
+import Nav from '@/components/Navigation'
 import Footer from '@/components/Footer'
 
 export default function Layout({ children, home }) {
