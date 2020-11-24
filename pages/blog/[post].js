b15a90761dfc3f3030510dac3df5849b98cbd714
Théophile Louvel <louveltheophile@gmail.com>
2020-11-24 05:48:44 +0300

    Adding dependency

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -32,25 +32,26 @@ export default function Post({ postData }) {
     const router = useRouter()
     return (
         <Layout>
-            <Head>
-                <title>{postData.title}</title>
-                <meta property="og:title" content={postData.title} />
-                <meta property="og:description" content={postData.description} />
-                <meta property="og:image" content={postData.cover} />
-                <meta property="og:url" content={router.pathname} />
-                <meta name="twitter:card" content={postData.cover} />
-                {/* Non-Essential, But Recommended */}
-                <meta property="og:site_name" content={siteInfo.title} />
-                {/* <meta name="twitter:image:alt" content="Alt text for image" /> */}
-                {/* Non-Essential, But Required for Analytics */}
-                {/* <meta property="fb:app_id" content="your_app_id" />
-                <meta name="twitter:site" content="@website-username" /> */}
-            </Head>
-            <article>
-                <h1 className="big-title">{postData.title}</h1>
-                <div>{postData.author}<Date dateString={postData.date} /></div>
-                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-            </article>
         </Layout>
     )
 }
+
+            // <Head>
+            //     <title>{postData.title}</title>
+            //     <meta property="og:title" content={postData.title} />
+            //     <meta property="og:description" content={postData.description} />
+            //     <meta property="og:image" content={postData.cover} />
+            //     <meta property="og:url" content={router.pathname} />
+            //     <meta name="twitter:card" content={postData.cover} />
+            //     {/* Non-Essential, But Recommended */}
+            //     <meta property="og:site_name" content={siteInfo.title} />
+            //     {/* <meta name="twitter:image:alt" content="Alt text for image" /> */}
+            //     {/* Non-Essential, But Required for Analytics */}
+            //     {/* <meta property="fb:app_id" content="your_app_id" />
+            //     <meta name="twitter:site" content="@website-username" /> */}
+            // </Head>
+            // <article>
+            //     <h1 className="big-title">{postData.title}</h1>
+            //     <div>{postData.author}<Date dateString={postData.date} /></div>
+            //     <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            // </article>
