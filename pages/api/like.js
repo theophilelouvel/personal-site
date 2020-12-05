d74261b8ae4537baed5c3ce54117dac4b50d1b0a
Théophile Louvel <louveltheophile@gmail.com>
2020-12-05 15:42:44 +0300

    :fire: Removing unused Sanity import

--- pages/api/like.js
+++ pages/api/like.js
@@ -1,13 +1,11 @@
-import { previewClient } from 'lib/sanity'
-
 export default async function like(req, res) {
-  const { id } = JSON.parse(req.body)
-  try {
-    await previewClient
-      .patch(id)
-      .inc({ likes: 1 })
-      .commit()
-  } catch (err) {
-    console.error(err)
-  }
+    const { id } = JSON.parse(req.body)
+    try {
+        await previewClient
+            .patch(id)
+            .inc({ likes: 1 })
+            .commit()
+    } catch (err) {
+        console.error(err)
+    }
 }
