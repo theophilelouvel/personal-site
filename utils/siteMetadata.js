39d1a782c69e3775ac03b1e0b8e2ad98d8bb9183
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-03-22 22:24:11 +0300

    debugging seo

--- utils/siteMetadata.js
+++ utils/siteMetadata.js
@@ -3,7 +3,7 @@ import { SiStackoverflow } from "react-icons/si";
 
 const siteInfo = {
     title: 'Théophile Louvel - Software Engineer',
-    description: 'I\'m a Software Engineer with a focus on Web Development from Paris, France. I code in Rust, Java and JavaScript and I speak French, English, Russian & Turkish.',
+    description: 'Software Engineer with a focus on Web Development from Paris, France.',
     author: 'Théophile Louvel',
     pronunciation: "te.ɔ.fil luvɛl",
     url: 'https://theophilelouvel.tech',
@@ -32,7 +32,7 @@ const socialLinks = [
     { name: 'twitter', href: 'https://twitter.com/flowsn4ke', icon: <FiTwitter /> },
     { name: 'stackoverflow', href: 'https://stackoverflow.com/users/14192686/th%c3%a9ophile-louvel', icon: <SiStackoverflow /> },
     // { name: 'youtube', href: 'https://www.youtube.com/channel/UCco896WsL_UlJeOecstyRAA', icon: <FiYoutube /> },
-    { name: 'github', href: 'https://github.com/flowsn4ke', icon: <FiGithub /> },
+    { name: 'github', href: 'https://github.com/theophilelouvel', icon: <FiGithub /> },
     { name: 'linkedin', href: 'https://www.linkedin.com/in/theophilelouvel', icon: <FiLinkedin /> },
     // { name: 'mail', href: 'mailto:louveltheophile@gmail.com', icon: <FiMail /> },
     { name: 'rss', href: siteInfo.url + '/rss.xml', icon: <FiRss /> },
