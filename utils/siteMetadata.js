37c13228e09a52fb63726097f29690865b83eb47
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-03-22 23:17:23 +0300

    debugging seo

--- utils/siteMetadata.js
+++ utils/siteMetadata.js
@@ -29,11 +29,11 @@ const navLinks = [
 ]
 
 const socialLinks = [
-    { name: 'twitter', href: 'https://twitter.com/flowsn4ke', icon: <FiTwitter /> },
-    { name: 'stackoverflow', href: 'https://stackoverflow.com/users/14192686/th%c3%a9ophile-louvel', icon: <SiStackoverflow /> },
+    { name: 'twitter', href: "https://twitter.com/flowsn4ke", icon: <FiTwitter /> },
+    // { name: 'stackoverflow', href: "https://stackoverflow.com/users/14192686/th%c3%a9ophile-louvel", icon: <SiStackoverflow /> },
     // { name: 'youtube', href: 'https://www.youtube.com/channel/UCco896WsL_UlJeOecstyRAA', icon: <FiYoutube /> },
-    { name: 'github', href: 'https://github.com/flowsn4ke', icon: <FiGithub /> },
-    { name: 'linkedin', href: 'https://www.linkedin.com/in/theophilelouvel', icon: <FiLinkedin /> },
+    { name: 'github', href: "https://github.com/flowsn4ke", icon: <FiGithub /> },
+    { name: 'linkedin', href: "www.linkedin.com/in/theolouvel", icon: <FiLinkedin /> },
     // { name: 'mail', href: 'mailto:louveltheophile@gmail.com', icon: <FiMail /> },
     { name: 'rss', href: siteInfo.url + '/rss.xml', icon: <FiRss /> },
 ]
