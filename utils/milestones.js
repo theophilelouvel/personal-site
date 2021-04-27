86d2255d7fd5263636df594d96e3becc178bcdfa
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-04-27 04:16:27 +0300

    broken urls

--- utils/milestones.js
+++ utils/milestones.js
@@ -1,74 +1,74 @@
 export const milestones = [
-    // {
-    //     link: "",
-    //     title: "",
-    //     date: "",
-    //     description: ""
-    // },
-    {
-        link: "https://dictionnaire-turc-francais.vercel.app/",
-        title: "Turkish -> French Dictionary App",
-        date: "2021-03-16",
-        description: "While I was living in Turkey a few years ago, I started to write a dictionnary for myself as I couldn't find a decent one. The project grew and grew, but it's not until recently that I acquired the necessary skill to make it available for others. It's now a tiny React app styled with TailwindCSS deployed on Vercel! There are many more entries to add, but the functionality's here! It supports fuzzy search and fetches definitions based on how much they share with the query string.",
-    },
-    {
-        link: "https://deus-foto.vercel.app/",
-        title: "Deus Foto Portfolio App",
-        date: "2020-12-08",
-        description: "It's not a typo! This is the portfolio I made for my Russian friend Denis, a most talented commercial photographer! It's made with Next.js using TailwindCSS and deployed on Vercel.",
-    },
-    {
-        link: "/blog/blog-building-and-yak-shaving",
-        title: "theophilelouvel.tech",
-        date: "2020-10-13",
-        description: "To get myself acquainted with Next.js, I set out to build this blog. At the same time, I used it to experiment working with Sanity and its Groq query language, which is a breeze, even though I came back to bare markdown articles later on as it was kind of overkill for my purpose. Click to read more about how I built this blog!",
-    },
-    {
-        link: "https://louvel-weather-app.herokuapp.com/",
-        title: "le Weather",
-        date: "2020-08-14",
-        description: "A small weather app written in vanilla JavaScript using the openweathermap & the ipinfo respective APIs. It first fetches the weather at your location, then you can look for any other location. Fuzzy search is also supported, making it more user-friendly. Deployed on Heroku. Click on this card to have a look!"
-    },
-    {
-        link: "https://louvel-random-word-generator.herokuapp.com/",
-        title: "Random word generator",
-        date: "2020-09-23",
-        description: "A vanilla JavaScript app that yields random words based on character frequencies in the English language and allows you to copy them to the clipboard. Click on this card to have a look! (Might take some time to load as it is deployed on a free plan on Heroku)"
-    },
-    {
-        link: "#",
-        title: "Switched careers towards software engineering",
-        date: "2020-04-01",
-        description: "The 2020 pandemic was the perfect occasion to make a switch I had been considering for some time already. I love languages, but unlike spoken languages, computer languages do scale!",
-    },
-    {
-        link: "https://theolouvel.medium.com/life-is-nothing-but-a-bike-ride-7da49315ea06",
-        title: "Paris -> St Petersburg on a bike",
-        date: "2014-8-10",
-        description: "An incredible adventure that took me from Paris, France to St. Petersburg, Russia. Click to read the article on Medium! (no paywalls)"
-    },
-    {
-        link: "#",
-        title: "Graduated from law school",
-        date: "2013-10-08",
-        description: "After school I studied international business law at la Sorbonne. Law is kind of society's programming when you think about it... At the same time I was learning Russian, a fascinating language, which eventually led me to work as a legal translator for the next few years.",
-    },
-    {
-        link: "#",
-        title: "First personal computer",
-        date: "2006-08-01",
-        description: "It had both a CD-ROM and a floppy disk reader! So much stuff! And so many hours spent on Age of Empires, Quake, Need for Speed and Total Annihilation!",
-    },
-    {
-        link: "#",
-        title: "First program in C++",
-        date: "2005-09-01",
-        description: "That year our school bought computers, which were running on Pentium III processors if I recall correctly. With some friends we set out to learn programming, which was made possible thanks to the now extinct Site du Zéro (which featured tutorials maintained by engineering students). I chose C++ because I found systems programming mesmerizing (and I still do). I remember making programs to perform basic read / write operations on files, but nothing more. I must say I didn't get far as I had only school breaks to practice!",
-    },
-    {
-        link: "#",
-        title: "First program in Basic",
-        date: "2004-09-01",
-        description: "The very first program I ever wrote was in Basic, on my old high school calculator Casio Graph 35+. Full disclosure: I had no idea what I was doing, I just really wanted to play Snake on the way home."
-    }
+	// {
+	//     link: "",
+	//     title: "",
+	//     date: "",
+	//     description: ""
+	// },
+	{
+		link: "https://dictionnaire-turc-francais.vercel.app/",
+		title: "Turkish -> French Dictionary App",
+		date: "2021-03-16",
+		description: "While I was living in Turkey a few years ago, I started to write a dictionnary for myself as I couldn't find a decent one. The project grew and grew, but it's not until recently that I acquired the necessary skill to make it available for others. It's now a tiny React app styled with TailwindCSS deployed on Vercel! There are many more entries to add, but the functionality's here! It supports fuzzy search and fetches definitions based on how much they share with the query string.",
+	},
+	{
+		link: "https://deus-foto.vercel.app/",
+		title: "Deus Foto Portfolio App",
+		date: "2020-12-08",
+		description: "It's not a typo! This is the portfolio I made for my Russian friend Denis, a most talented commercial photographer! It's made with Next.js using TailwindCSS and deployed on Vercel.",
+	},
+	{
+		link: "/blog/blog-building-and-yak-shaving",
+		title: "theolouvel.dev",
+		date: "2020-10-13",
+		description: "To get myself acquainted with Next.js, I set out to build this blog. At the same time, I used it to experiment working with Sanity and its Groq query language, which is a breeze, even though I came back to bare markdown articles later on as it was kind of overkill for my purpose. Click to read more about how I built this blog!",
+	},
+	{
+		link: "https://louvel-weather-app.herokuapp.com/",
+		title: "le Weather",
+		date: "2020-08-14",
+		description: "A small weather app written in vanilla JavaScript using the openweathermap & the ipinfo respective APIs. It first fetches the weather at your location, then you can look for any other location. Fuzzy search is also supported, making it more user-friendly. Deployed on Heroku. Click on this card to have a look!"
+	},
+	{
+		link: "https://louvel-random-word-generator.herokuapp.com/",
+		title: "Random word generator",
+		date: "2020-09-23",
+		description: "A vanilla JavaScript app that yields random words based on character frequencies in the English language and allows you to copy them to the clipboard. Click on this card to have a look! (Might take some time to load as it is deployed on a free plan on Heroku)"
+	},
+	{
+		link: "#",
+		title: "Switched careers towards software engineering",
+		date: "2020-04-01",
+		description: "The 2020 pandemic was the perfect occasion to make a switch I had been considering for some time already. I love languages, but unlike spoken languages, computer languages do scale!",
+	},
+	{
+		link: "https://theolouvel.medium.com/life-is-nothing-but-a-bike-ride-7da49315ea06",
+		title: "Paris -> St Petersburg on a bike",
+		date: "2014-8-10",
+		description: "An incredible adventure that took me from Paris, France to St. Petersburg, Russia. Click to read the article on Medium! (no paywalls)"
+	},
+	{
+		link: "#",
+		title: "Graduated from law school",
+		date: "2013-10-08",
+		description: "After school I studied international business law at la Sorbonne. Law is kind of society's programming when you think about it... At the same time I was learning Russian, a fascinating language, which eventually led me to work as a legal translator for the next few years.",
+	},
+	{
+		link: "#",
+		title: "First personal computer",
+		date: "2006-08-01",
+		description: "It had both a CD-ROM and a floppy disk reader! So much stuff! And so many hours spent on Age of Empires, Quake, Need for Speed and Total Annihilation!",
+	},
+	{
+		link: "#",
+		title: "First program in C++",
+		date: "2005-09-01",
+		description: "That year our school bought computers, which were running on Pentium III processors if I recall correctly. With some friends we set out to learn programming, which was made possible thanks to the now extinct Site du Zéro (which featured tutorials maintained by engineering students). I chose C++ because I found systems programming mesmerizing (and I still do). I remember making programs to perform basic read / write operations on files, but nothing more. I must say I didn't get far as I had only school breaks to practice!",
+	},
+	{
+		link: "#",
+		title: "First program in Basic",
+		date: "2004-09-01",
+		description: "The very first program I ever wrote was in Basic, on my old high school calculator Casio Graph 35+. Full disclosure: I had no idea what I was doing, I just really wanted to play Snake on the way home."
+	}
 ];
