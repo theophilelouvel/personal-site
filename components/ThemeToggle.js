b7569910128a4d27f6e46a596889cc26ebf6fc81
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-05-04 19:29:18 +0300

    more cleanup

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -1,12 +1,8 @@
-import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes'
 import { FiSun, FiMoon } from 'react-icons/fi'
 
 export default function ThemeToggle() {
-	const [mounted, setMounted] = useState(false);
 	const { theme, setTheme } = useTheme();
-
-	useEffect(() => setMounted(true), []);
 
 	return (
 		<button
@@ -15,7 +11,7 @@ export default function ThemeToggle() {
 			className="bg-gradient-to-br from-orange-500 to-orange-600 rounded p-3 h-10 w-10 focus:outline-none flex items-center justify-center text-center"
 			onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
 		>
-			{mounted && theme === 'dark' ? <FiSun size="22" className="text-bluegray-50" /> : <FiMoon size="22" className="text-bluegray-50" />}
+			{theme === 'dark' ? <FiSun className="text-bluegray-50" /> : <FiMoon className="text-bluegray-50" />}
 		</button>
 	);
 }
