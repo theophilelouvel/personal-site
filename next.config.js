078a00ace38be40d017665c2a4e85728821c7564
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-05-13 06:10:43 +0300

    implemented preact

--- next.config.js
+++ next.config.js
@@ -1,25 +1,40 @@
 module.exports = {
-    images: {
-        domains: ['source.unsplash.com'],
-    },
-    async rewrites() {
-        return [
-            {
-                source: "/sitemap.xml",
-                destination: "/api/sitemap.xml",
-            },
-            {
-                source: "/feed",
-                destination: "/api/rss.xml",
-            },
-            {
-                source: "/rss",
-                destination: "/api/rss.xml",
-            },
-            {
-                source: "/rss.xml",
-                destination: "/api/rss.xml",
-            },
-        ];
-    },
+	future: {
+		webpack5: true,
+	},
+	images: {
+		domains: ['source.unsplash.com'],
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/sitemap.xml",
+				destination: "/api/sitemap.xml",
+			},
+			{
+				source: "/feed",
+				destination: "/api/rss.xml",
+			},
+			{
+				source: "/rss",
+				destination: "/api/rss.xml",
+			},
+			{
+				source: "/rss.xml",
+				destination: "/api/rss.xml",
+			},
+		];
+	},
+	webpack: (config, { dev, isServer }) => {
+		// Replace React with Preact only in client production build
+		if (!dev && !isServer) {
+			Object.assign(config.resolve.alias, {
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat',
+			});
+		}
+
+		return config;
+	},
 }
